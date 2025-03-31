3cbc773c844ac24be3291eff91d8743ae44d2ccd
devtuanamit1409 <nguyentuanamit@gmail.com>
2025-03-31 20:54:47 +0700

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: "strapi-thang-long-xua",
+      script: "npm",
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
